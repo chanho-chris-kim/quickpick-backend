58d19d4033758aed66391e1d7b0b008553893247
Chanho Kim <ckim@tvo.org>
2025-08-17 11:25:05 -0400

    java backend init

---
+++ src/main/java/com/example/quickpick_backend/QuickpickBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.quickpick_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickpickBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickpickBackendApplication.class, args);
+	}
+
+}
